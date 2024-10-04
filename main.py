414a4650f79efb23155cbb52d71e5d95e6e91bd0
Devon <ds.pydev@gmail.com>
2024-10-04 10:38:09 +0200

    Add functionality

---
+++ main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+data = pd.read_csv('1 - Resource/coffee.csv', skip_blank_lines=True, dtype=str, keep_default_na=False)
+
+# Extract columns and strip white space
+coffee_extract = data.iloc[:, [1, 6]].values.tolist()
+
+coffee_extracted_list = []
+coffee_extracted_list_names = []
+
+for x in coffee_extract:
+    name = x[0].strip()
+    quantity = x[1].strip()
+    if name != 'Stock Description':
+        if name != '':
+            coffee_extracted_list.append([name, quantity])
+            coffee_extracted_list_names.append(name)
+
+# Remove duplicate
+coffee_names_filtered = list(set(coffee_extracted_list_names))
+
+# Create dic and add totals 
+coffees = {}
+
+for x in coffee_names_filtered:
+    if x !='':
+        coffees[x] = 0
+    
+for x in coffee_extracted_list:
+        coffees[x[0]] += float(x[1])
+
+
+
+
+
+
+
+
+
+'''
+'ROOIBOS TEA 250ML'
+'LAVAZZA AMERICANO W/MILK 3'
+'LAVAZZA ICED COFFEE 350ML'
+'BEAN COFFEE MACHINE'
+'LAVAZZA AMERI W/MILK 250ML'
+'HOT CHOCOLATE 350ML'
+'LAVAZZA ESPRESSO'
+'LAVAZZA AMERICANO 250ML'
+'LAVAZZA CAPPUCCINO 250ML'
+'LAVAZZA CAPPUCCINO 350ML'
+'LAVAZZA AMERICANO 350ML'
+'''
